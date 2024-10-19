503d736d6131e3a5962a690093f94ddac73d05fc
BhavyaSinghalcode <bhavyasinghal134@gmail.com>
2024-10-19 19:49:08 +0530

    Add files via upload

---
+++ swap numbers.py
@@ -0,0 +1,16 @@
+# Given inputs
+x = 5
+y = 9
+z = 1
+
+# Creating a list of the variables
+numbers = [x, y, z]
+
+# Sorting the list in ascending order
+numbers.sort()
+
+# Assigning the sorted values back to x, y, z
+x, y, z = numbers
+
+# Printing the updated values
+print(f"x = {x}, y = {y}, z = {z}")
